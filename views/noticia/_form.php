a599ab33fa5b7da35535ec4f932c8b5e3d4c0540
Gian Dal Pont <giandalpont@gmail.com>
2020-10-25 12:16:43 -0300

    criado crud da notícia

---
+++ views/noticia/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Noticia */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="noticia-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'titulo')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'cabeca')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'corpo')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'statu')->textInput() ?>
+
+    <?= $form->field($model, 'create_at')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
